276124913e9a008d8f87782d4bab0fbe0d47c3c8
ArathiSekar <64216601+ArathiSekar@users.noreply.github.com>
2022-09-19 10:47:18 -0700

    Create Implement stack using list.py

---
+++ Implement stack using list.py
@@ -0,0 +1,36 @@
+# Build a Stack using a list where the first element is at the bottom of our stack
+
+class Stack:
+    def __init__(self):
+        self.items = []
+        
+    def size(self):
+        return (len(self.items))
+    
+    def push(self, item):
+        self.items.append(item)
+        
+    def pop(self):
+        if self.size()==0:
+            return None
+        else:
+            self.items.pop()
+
+MyStack = Stack()
+
+MyStack.push("Web Page 1")
+MyStack.push("Web Page 2")
+MyStack.push("Web Page 3")
+
+print (MyStack.items)
+
+MyStack.pop()
+
+print ("Pass" if (MyStack.items[0] == 'Web Page 1') else "Fail")
+
+MyStack.pop()
+
+print ("Pass" if (MyStack.pop() == None) else "Fail")
+MyStack.pop()
+
+print (MyStack.items)
